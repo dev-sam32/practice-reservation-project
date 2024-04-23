5a893f289440190d45253712f50668091e2d74f2
dev-sam32 <shinhs91@gmail.com>
2024-04-23 18:19:46 +0900

    [Feat] password 저장 및 인증시 Encode, Decode - PasswordEncoder   - BCryptPasswordEncoder
    - Member
      - Column 길이 제한 수정 50 > 60
    
    -

--- src/main/java/com/zerobase/reservation/service/MemberService.java
+++ src/main/java/com/zerobase/reservation/service/MemberService.java
@@ -6,35 +6,45 @@ import com.zerobase.reservation.entity.Member;
 import com.zerobase.reservation.repository.MemberRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @AllArgsConstructor
 @Service
-public class MemberService {
+public class MemberService implements UserDetailsService {
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
 
     public Member register(SignUp signUp) {
         boolean exists = this.memberRepository.existsByName(signUp.name());
+
         if (!exists) {
                 Member member = signUp.toEntity();
-                member.setPassword(signUp.password());
-
+                member.setPassword(this.passwordEncoder.encode(signUp.password()));
                 return this.memberRepository.save(member);
         } else {
             // TODO : Exception
-            throw new RuntimeException("이미 존재하는 ID 입니다." + signUp.name());
+            throw new RuntimeException("이미 존재하는 ID 입니다. -> " + signUp.name());
         }
     }
 
     public Member authentication(SignIn signIn) {
-        Member member = this.memberRepository.findByName(signIn.name())
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 ID 입니다."));
+        Member member = (Member) loadUserByUsername(signIn.name());
 
-        if (!member.getPassword().equals(signIn.password())) {
+        if (!this.passwordEncoder.matches(member.getPassword(), signIn.password())) {
             throw new RuntimeException("비밀번호가 일치하지 않습니다.");
         }
 
         return member;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.memberRepository.findByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("계정을 찾을 수 없습니다. -> " + username));
     }
 }
