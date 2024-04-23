52ddf3d2effa73f73196c55d8e8a7521a06da785
dev-sam32 <shinhs91@gmail.com>
2024-04-23 17:45:37 +0900

    [Feat] Spring Security 기본 설정 - SecurityConfig   - h2 DB 접속   - 모든 권한 허용   추후 인가 부분 수정 예정 - data.sql   - admin 계정 생성 설정   추후 ADMIN 계정으로 모든 권한을 받아, 테스트 하기 위함 - Member   - role, getAuthorities() 수정   일반 사용자와 파트너 엔티티를 따로 구분 했었는데, 비효율적인 것 같아서 권한으로 구분하기로 함
    -

--- src/main/java/com/zerobase/reservation/controller/AuthController.java
+++ src/main/java/com/zerobase/reservation/controller/AuthController.java
@@ -2,10 +2,8 @@ package com.zerobase.reservation.controller;
 
 import com.zerobase.reservation.dto.Auth.SignIn;
 import com.zerobase.reservation.dto.Auth.SignUp;
-import com.zerobase.reservation.entity.MemberEntity;
-import com.zerobase.reservation.entity.PartnerEntity;
+import com.zerobase.reservation.entity.Member;
 import com.zerobase.reservation.service.MemberService;
-import com.zerobase.reservation.service.PartnerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,33 +15,20 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 public class AuthController {
     private final MemberService memberService;
-    private final PartnerService partnerService;
 
     @PostMapping("/signup")
     public ResponseEntity<?> signUp(@RequestBody SignUp req) {
-        if(req.partnerYn()) {
-            PartnerEntity result = this.partnerService.register(req);
-            log.info("Partner SignUp!!!");
-            return ResponseEntity.ok(result);
-        } else {
-            MemberEntity result = this.memberService.register(req);
-            log.info("Member SignUp!!!");
-            return ResponseEntity.ok(result);
-        }
+        Member result = this.memberService.register(req);
+
+        log.info("Member SignUp!!!");
+        return ResponseEntity.ok(result);
     }
 
     @GetMapping("/signin")
     public ResponseEntity<?> signIn(@RequestBody SignIn req) {
-        if(req.partnerYn()) {
-            PartnerEntity result = this.partnerService.authentication(req);
+        Member result = this.memberService.authentication(req);
 
-            log.info("Partner SignIn!!!");
-            return ResponseEntity.ok(result);
-        } else {
-            MemberEntity result = this.memberService.authentication(req);
-
-            log.info("Member SignIn!!!");
-            return ResponseEntity.ok(result);
-        }
+        log.info("Member SignIn!!!");
+        return ResponseEntity.ok(result);
     }
 }
---
+++ src/main/java/com/zerobase/reservation/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package com.zerobase.reservation.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(
+                        headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
+                )
+                .authorizeHttpRequests(
+                        req -> req.anyRequest().permitAll()
+                )
+                .sessionManagement(
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
+
+        return http.build();
+    }
+}
